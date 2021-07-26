5c0c873ba4b59fbe184d4bb025e48d43231e0df0
SIDDHANT DEVIL <83235714+unknownforall1@users.noreply.github.com>
2021-07-26 23:49:20 +0530

    Update siddhantt.py

--- siddhantt.py
+++ siddhantt.py
@@ -1069,25 +1069,26 @@ async def spam(e):
     if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
-        yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(2," ", 1)
         smex = await e.get_reply_message()
         if len(yukki) == 2:
             message = str(yukki[1])
             counter = int(yukki[0])
-            if counter > 100:
+            chat_id = int(yukki[2])
+            if counter > 100000000:
                 return await e.reply(error, parse_mode=None, link_preview=None )
             await asyncio.wait([e.respond(message) for i in range(counter)])
         elif e.reply_to_msg_id and smex.media:  
             counter = int(yukki[0])
-            if counter > 100:
+            if counter > 100000000:
                 return await e.reply(error, parse_mode=None, link_preview=None )
             for _ in range(counter):
-                smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                smex = await e.client.send_file(chat_id, smex, caption=smex.text)
                 await gifspam(e, smex)  
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
             counter = int(yukki[0])
-            if counter > 100:
+            if counter > 100000000:
                 return await e.reply(error, parse_mode=None, link_preview=None )
             await asyncio.wait([e.respond(message) for i in range(counter)])
         else:
